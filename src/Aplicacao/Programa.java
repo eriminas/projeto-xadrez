07a71acbf2032a05c5f07812afbf1df51112f233
“eriminas” <eriminas@hotmail.com>
2023-01-02 16:26:34 -0300

    Firt class: Position

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -1,6 +1,9 @@
 package Aplicacao;
+import boardgame.Position;
+
 public class Programa {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }    
 }
