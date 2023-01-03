c71cb9b923330e82838a70ad76d0618c995347d3
“eriminas” <eriminas@hotmail.com>
2023-01-03 14:09:20 -0300

    Continuing the implementation to Board and Piece

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -1,9 +1,12 @@
 package Aplicacao;
+import boardgame.Board;
 import boardgame.Position;
 
 public class Programa {
     public static void main(String[] args) {
         Position pos = new Position(3, 5);
         System.out.println(pos);
+        
+        Board board = new Board(8, 8);
     }    
 }
