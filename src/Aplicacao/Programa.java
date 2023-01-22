e82e298b32d584769cff4afe9e98d2535ea6420d
“eriminas” <eriminas@hotmail.com>
2023-01-22 17:47:05 -0300

    Quais são os possiveis movimentos de uma peca?

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -22,6 +22,10 @@ public class Programa {
                 System.out.println();
                 System.out.println("Origem/Source: ");
                 ChessPosition source = UI.readChessPosition(entrada);
+                
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
 
                 System.out.println();
                 System.out.println("Destino/Target: ");
